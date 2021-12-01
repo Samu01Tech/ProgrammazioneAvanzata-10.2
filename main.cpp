430bb28d2b66e0e9d13e0351f7198734cbedccf6
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-12-01 16:23:56 +0000

    mutex (lock + unlock)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <thread>
-//#include <mutex>
+#include <mutex>
 using namespace std;
 
+mutex mtx;
 class Test{
     private: 
         int dato;
@@ -15,10 +16,26 @@ class Test{
         }
 };
 
+void stampa(int dim, char c){
+    mtx.lock();
+    for(int i=0; i<dim; i++){
+        cout << c;
+    }
+    mtx.unlock();
+}
+
 int main() {
     Test miotest;
     thread tc(Test::stampa, &miotest, 5);
     thread tpar(miotest, 9);
     tc.join();
     tpar.join();
+    
+
+    /*
+    thread t1(stampa, 20, 'A');
+    thread t2(stampa, 10, 'Z');
+    t1.join();
+    t2.join();
+    */
 } 
