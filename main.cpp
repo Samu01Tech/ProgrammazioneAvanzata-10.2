ebcee787e3cea6db48f9fb9c1fcac5d82656662e
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-12-01 16:05:50 +0000

    introduzione

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <thread>
+using namespace std;
+
+void stampa(int dato){
+    cout << dato;
+}
+
+void stampa2(){
+    cout << "Nulla";
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+    thread stampaThread(stampa, 5);
+    thread stampaNulla(stampa2);
+    cout << "Ciao";
+    stampaNulla.join();
+    stampaThread.join();
+    cout << "Fine";
 } 
